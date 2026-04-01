9fc907f4dc3148894a183fd45e357da42fd687a4
RafaJ <rmarmor406@g.educaand.es>
2026-04-01 12:22:30 +0200

    260401 sistema de enrutamiento terminado

--- src/Controllers/BibliotecaController.php
+++ src/Controllers/BibliotecaController.php
@@ -1,6 +1,17 @@
 <?php
+require_once __DIR__ . '/../Models/Obra.php';
+require_once __DIR__ . '/../Models/Autor.php';
+require_once __DIR__ . '/../Models/Comentario.php';
+require_once __DIR__ . '/../Modedels/Puntuacion.php';
+require_once __DIR__ . '/../Modedels/Etiqueta.php';
 
 class BibliotecaController {
-    
+    public function catalogo() {
+        $busqueda=$_GET['busqueda'] ?? null;
+        $genero=$_GET['genero'] ?? null;
+        $autor=$_GET['autor'] ?? null;
+        $pagina=$_GET['pagina'] ?? 1;
+        $porPagina=$_GET['porPagina'] ?? 16;
+    }
 }
 ?>
